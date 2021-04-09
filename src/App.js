355f807511396094bcfcbdfa33ac0160ac90a948
Ashenone-62 <2371775802@qq.com>
2021-04-09 22:16:18 +0800

    渲染list与保存本地localStorage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import AddInput from './components/AddInput';
+import TodoItem from './components/TodoItem';
 
 function App() {
 
   const [ isInputShow, setInputShow ] = useState(false);
   const [ todoList, setTodoList ] = useState([]);
+
+  /* 注意顺序 */ 
+  useEffect(() => {
+    const todoData = JSON.parse(localStorage.getItem('todoData') || '[]')
+    setTodoList(todoData)
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('todoData', JSON.stringify(todoList))
+  }, [ todoList ]);
 
   /* 防止子组件因为父组件更新，子组件识别additem是新的而变化重绘 */ 
   const addItem = useCallback((value) => {
@@ -19,12 +30,22 @@ function App() {
     setTodoList(( todoList )=> [ ...todoList, dataItem ]);
 
     setInputShow(false)
-  },[]);
+  }, []);
 
   return (
     <div className="App">
       <Header openInput={ () => { setInputShow(!isInputShow) } } />
       <AddInput  isInputShow={ isInputShow } addItem={ (value) => { addItem(value) } }  />
+      <ul className="todo-list">
+        {
+          todoList.map((item,index) => {
+
+            return (
+              <TodoItem data={ item } key={ index } />
+            );
+          })
+        }
+      </ul>
     </div>
   );
 }
